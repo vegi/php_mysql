e9e87d232d8fa2e4d4b1a5ad48e474859d1d543d
Jerome Koller <jerome.koller@gmx.ch>
2013-11-19 00:00:57 +0100

    Uebung10 Trackstar

---
+++ Uebungen/Uebung_10/trackstar/protected/tests/unit/DbTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class DbTest extends CTestCase {
+    public function testConnection () {
+        $this->assertNotNull(
+            Yii::app()->db->connectionString
+            );
+    }   
+}
